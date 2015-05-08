8a48f20ea2544763e2ff889311dc8c4a04cc324e
stenvala <antti@stenvall.fi>
2015-05-08 09:25:25 +0300

    Added middleware to all

--- all.php
+++ all.php
@@ -6,5 +6,4 @@ require_once __DIR__ . '/serviceName.php';
 require_once __DIR__ . '/serviceDescription.php';
 require_once __DIR__ . '/route.php';
 require_once __DIR__ . '/helpTxt.php';
-
-?>
+require_once __DIR__ . '/middleware.php';
